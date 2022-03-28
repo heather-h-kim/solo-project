072bfbb4115a5713a45a9747a0372d3224ed3f37
Heather Kim <heatherkim0405@gmail.com>
2022-03-27 22:18:42 -0500

    revised the about page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -13,7 +13,7 @@ function About(props) {
   return (
     <div className="container">
       <h2>Technologies used</h2>
-      <ul style={{fontSize: 20}} >
+      <ul style={{fontSize: 19}} >
         <li >Node</li>
         <li>Express</li>
         <li>React</li>
@@ -23,12 +23,12 @@ function About(props) {
         <li>Material-UI</li>
       </ul>
       <h2>Challenges</h2>
-      <ul>
+      <ul style={{fontSize: 19}}>
         <li>Figuring out SQL queries to do conditional calculations - the recommended calories change depending on cat's age and neuter status.<br></br></li>
-        <li>Figuring out how to send multiple http requests for the chained calculations for each combination of foods</li>
+        <li>Figuring out how to send data and multiple http requests for the chained calculations for each combination of foods</li>
       </ul>
       <h2>What's Next?</h2>
-       <p>Adding a graph for a visual representation of the cat's weight change.</p>
+       <p style={{fontSize: 19}}>Adding a graph for a visual representation of the cat's weight change.</p>
     </div>
   );
 }
